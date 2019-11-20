3d59af64e756f4de4e93e867422b59a5199e5b96
Juhi Nayak <juhinayak07@gmail.com>
2019-11-20 13:02:54 -0800

    database queries

--- client/src/components/DoctorRegister.js
+++ client/src/components/DoctorRegister.js
@@ -1,110 +1,138 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 
-export default class DoctorRegister extends Component {
+class DoctorRegister extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
             Degree: "",
             Specialization: "",
             YearOfExperience: "",
             Address: "",
             Contact: "",
-            profile:"",
-
+            Fee:"",
+            Gender:"",
+            errors: {}
         };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+      }
+
+      onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+      }
+      onSubmit(e) {
+        e.preventDefault();
+    
+        const docProfile = {
+          Degree: this.state.Degree,
+          Specialization: this.state.Specialization,
+          YearOfExperience: this.state.YearOfExperience,
+          Address: this.state.Address,
+          Contact: this.state.Contact,
+          Fee: this.state.Fee,
+          Gender: this.state.Gender
+        };
+        console.log("Doctor info ",docProfile);
+       /* register(newUser).then(res => {
+          alert("Please Login with this ID "+ res);
+          this.props.history.push(`/login`);
+        });*/
       }
     render() {
         return (
-            
             <div className="container">
-                <h2>Please fill the form to complete your profile</h2>
-                <form onSubmit={this.handleSubmit}>
-                <div class="form-row">
-                    <div className="form-group col-md-5">
-                    <input 
-                        className="form-control" 
-                        type="text"
-                        id="Degree"
-                        placeholder="Enter your Degree"
-                        value={this.state.Degree}
-                        onChange={this.onInputChange}
-                    />
-                    </div>
-                </div>
-                <div class="form-row">            
-                    <div className="form-group col-md-5">
-                    <input 
-                    className="form-control" 
-                    type="text"
-                    id="Specialization"
-                    placeholder="Enter your Specialization"
-                    value={this.state.Specialization}
-                    onChange={this.onInputChange}
-                    />
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div className="form-group col-md-5">
-                    <input 
-                        className="form-control" 
-                        type="text"
-                        id="YearOfExperience"
-                        placeholder="Enter total year of experience"
-                        value={this.state.YearOfExperience}
-                        onChange={this.onInputChange}
-                    />
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div className="form-group col-md-5">
-                    <input 
-                        className="form-control" 
-                        type="text"
-                        id="Address"
-                        placeholder="Enter Address"
-                        value={this.state.Address}
-                        onChange={this.onInputChange}
-                    />
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div className="form-group col-md-5">
-                    <input 
-                        className="form-control" 
-                        type="text"
-                        id="Contact"
-                        placeholder="Enter your Contact"
-                        value={this.state.Contact}
-                        onChange={this.onInputChange}
-                    />
-                    </div>
-                </div>
-                <div class="form-row">
-                <div className="form-group col-md-2">
-                    <label for="profile">
-                    Profile Type
-                    </label>
-                </div>
-                    <div className="form-group col-md-3">
-                    <select className="form-control" id="profile" value={this.state.value} onChange={this.onInputChange} 
-                         placeholder="Enter usertype">
+        <div className="row">
+          <div className="col-md-6 mt-5 mx-auto" style={{textAlign:'left'}}>
+            <form noValidate onSubmit={this.onSubmit}>
+              <h1 className="h3 mb-3 font-weight-normal">Please fill the form to complete your profile</h1>
+              <div className="form-group">
+                <label htmlFor="Degree">Degree</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="Degree"
+                  placeholder="Enter your Degree"
+                  value={this.state.Degree}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="Specialization">Specialization</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="Specialization"
+                  placeholder="Enter your Specialization"
+                  value={this.state.Specialization}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="YearOfExperience">Experience</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="YearOfExperience"
+                  placeholder="Enter total year of Experience"
+                  value={this.state.YearOfExperience}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="Address">Address</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="Address"
+                  placeholder="Enter clinic Address"
+                  value={this.state.Address}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="Contact">Contact</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="Contact"
+                  placeholder="Enter Contact number"
+                  value={this.state.Contact}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="Fee">Fee</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="Fee"
+                  placeholder="Enter consultancy charge"
+                  value={this.state.Fee}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+              <label htmlFor="Gender">Gender</label>
+                    <select className="form-control" name="Gender" value={this.state.value} onChange={this.onChange}>
                         <option value="default" defaultValue>Select</option>  
-                        <option value="doctor">Doctor</option>
-                        <option value="patient">Patient</option>
-                        <option value="cashier">Cashier</option>
+                        <option value="F">Female</option>
+                        <option value="M">Male</option>
                     </select>
                 </div>
-                </div>
-                <div className="form-group col-md-3">
-                    <button className="btn btn-primary" type="submit">
-                    Update Profile
-                    </button>
-                </div>
+              <button
+                type="submit"
+                className="btn btn-lg btn-primary btn-block">
+                Upload Profile!
+              </button>
             </form>
-            </div>
+          </div>
+        </div>
+      </div>
         
         )
     }
 }
+
+export default DoctorRegister;
