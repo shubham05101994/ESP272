9bf9cce0d8d504c70b30134360b658315a5d10f1
Juhi Nayak <juhinayak07@gmail.com>
2019-11-19 17:46:08 -0800

    Initial commit

---
+++ client/src/components/DoctorRegister.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class DoctorRegister extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Please fill the form to complete your profile</h2>
+
+            </div>
+        )
+    }
+}
