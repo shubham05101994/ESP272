5cc8ea732470a89c3429b3a24769ad27dd78fd82
shubham <shubhamkatariya05@gmail.com>
2019-11-25 01:59:43 -0800

    check

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.json()) ;
-app.use(cors());
+app.use(cors()); 
 app.use(
   express.urlencoded({
     extended: false
