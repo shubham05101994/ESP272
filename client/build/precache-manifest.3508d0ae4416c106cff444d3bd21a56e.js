e6635bce954003b8536ad94a84ef355cc7c14a77
shubham <shubhamkatariya05@gmail.com>
2019-11-24 22:20:31 -0800

    Bild project

---
+++ client/build/precache-manifest.3508d0ae4416c106cff444d3bd21a56e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881dbe6ea09fc2421a62d25957a63966",
+    "url": "/index.html"
+  },
+  {
+    "revision": "304ae1b6778341606646",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "5886f0aa0ef07d463bca",
+    "url": "/static/css/main.b351179c.chunk.css"
+  },
+  {
+    "revision": "304ae1b6778341606646",
+    "url": "/static/js/2.d1e43e40.chunk.js"
+  },
+  {
+    "revision": "5886f0aa0ef07d463bca",
+    "url": "/static/js/main.8d67bc8c.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require("express");
+var path = require("path");
+
 var cors = require("cors");
 //var bodyParser = require('body-parser')
 var app = express();
 var port = process.env.PORT || 5000;
-
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.json()) ;
 app.use(cors());
 app.use(
